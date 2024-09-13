15a010acfcbc7d1bfeb1b15742f36b3bbf7379ef
sharkby7e <sgquin@gmail.com>
2024-09-13 18:39:28 -0400

    don't use the component that is calling prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
             </p>
           </div>
 
-          <LatestPost />
+          {/* <LatestPost /> */}
         </div>
       </main>
     </HydrateClient>
