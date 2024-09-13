f81c85c952e5d1ebe74896f2421dbf1c1093a0b2
David Wilkins <wilkidav@gmail.com>
2024-09-13 18:50:58 -0400

    added hello back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
