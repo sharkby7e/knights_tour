1f73ed2c5085afd0401e1a04c777dc81da7c4419
sharkby7e <sgquin@gmail.com>
2024-09-14 00:57:10 -0400

    see if trpc still works when rendering and add instructions to readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default async function Home() {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
+              {hello ? hello.greeting : "Loading tRPC query..."}
             </p>
           </div>
 
